e7374f412a913597a1706b56e63e39e4d8091442
prenas <prenas@gmail.com>
2021-11-18 00:18:21 -0300

    Defino clase que convierte divisas e imprime las consultas en consola

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-var solicitudes = [{ monto: 100, moneda: 'USD' }, { monto: 100, moneda: 'CAN' }];
-var cotizaciones = [{ moneda: 'USD', cotizacion: 200 }, { moneda: 'CAN', cotizacion: 120 }];
+var solicitudes = [{ monto: 100, moneda: 'USD' }, { monto: 200, moneda: 'CAN' }];
+var cotizaciones = [
+    { moneda: 'USD', cotizacionVenta: 200, cotizacionCompra: 195 },
+    { moneda: 'CAN', cotizacionVenta: 120, cotizacionCompra: 110 }
+];
+// Clase que tiene un método de consulta las cotizaciones de la moneda solicitada y una vez realizada
+// retorna los valores de la divisas correspondientes a la consulta.
+var ConversionDivisa = /** @class */ (function () {
+    function ConversionDivisa(monto, moneda) {
+        this.monto = monto;
+        this.moneda = moneda;
+    }
+    ConversionDivisa.prototype.consulta = function () {
+        var _this = this;
+        var consulta = cotizaciones.find(function (cotizacion) { return cotizacion.moneda === _this.moneda; });
+        return {
+            montoOriginal: this.monto,
+            moneda: this.moneda,
+            valorCompra: consulta.cotizacionCompra,
+            totalCompra: consulta.cotizacionCompra * this.monto,
+            valorVenta: consulta.cotizacionVenta,
+            totalVenta: consulta.cotizacionVenta * this.monto
+        };
+    };
+    return ConversionDivisa;
+}());
+// Ciclo que lee cada una de las solicitudes y llama a la clase que hace la conversión de divisas y 
+// Luego hace la impresión del objeto en pantalla. 
+solicitudes.forEach(function (solicitud) {
+    console.log(new ConversionDivisa(solicitud.monto, solicitud.moneda).consulta());
+});
--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
-let solicitudes : Solicitud[] = [{monto:100, moneda : 'USD'}, {monto : 100, moneda : 'CAN'}]
-let cotizaciones : Cotizaciones[] = [{moneda: 'USD', cotizacion : 200 }, { moneda : 'CAN', cotizacion : 120 }]
+let solicitudes : Solicitud[] = [{monto:100, moneda : 'USD'}, {monto : 200, moneda : 'CAN'}]
+let cotizaciones : Cotizaciones[] = [
+                                     {moneda: 'USD', cotizacionVenta : 200, cotizacionCompra : 195 },
+                                     { moneda : 'CAN', cotizacionVenta : 120 , cotizacionCompra : 110}
+                                    ]
 
 interface Solicitud {
     monto : number,
@@ -8,5 +11,36 @@ interface Solicitud {
 
 interface Cotizaciones {
     moneda : string,
-    cotizacion : number
+    cotizacionVenta : number,
+    cotizacionCompra : number
 }
+
+// Clase que tiene un método de consulta las cotizaciones de la moneda solicitada y una vez realizada
+// retorna los valores de la divisas correspondientes a la consulta.
+class ConversionDivisa {
+    monto
+    moneda
+    constructor( monto , moneda) {
+        this.monto = monto
+        this.moneda = moneda
+    }
+    
+    consulta(){
+        let consulta = cotizaciones.find( cotizacion => cotizacion.moneda === this.moneda )
+        return  { 
+                  montoOriginal : this.monto,
+                  moneda : this.moneda,
+                  valorCompra : consulta.cotizacionCompra,
+                  totalCompra : consulta.cotizacionCompra * this.monto, 
+                  valorVenta : consulta.cotizacionVenta,
+                  totalVenta : consulta.cotizacionVenta * this.monto
+                } 
+    }
+}
+
+// Ciclo que lee cada una de las solicitudes y llama a la clase que hace la conversión de divisas y 
+// Luego hace la impresión del objeto en pantalla. 
+
+solicitudes.forEach( solicitud => {
+    console.log(  new ConversionDivisa( solicitud.monto, solicitud.moneda ).consulta() )
+});
